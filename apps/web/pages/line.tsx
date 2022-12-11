406e169d2a7c3b31a016caeb5351c6a2f271cc7f
DrowningToast <supratouchgussii@gmail.com>
2022-12-11 15:25:45 +0700

    fix: logo

--- apps/web/pages/line.tsx
+++ apps/web/pages/line.tsx
@@ -1,11 +1,19 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import MobileFrame from "../components/MobileFrame";
 import Navbar from "../components/Navbar";
 
 const LinePage: NextPage = () => {
+  const router = useRouter();
+
+  console.log(router.query.district);
+
   return (
     <MobileFrame>
-      <Navbar />
+      <div className="">
+        <Navbar />
+      </div>
       <div className="w-full flex flex-col items-center">
         <div className="flex flex-col w-full h-full gap-y-4 text-center">
           <h1 className="text-2xl text-[#49C628] font-kanit font-bold">
@@ -15,6 +23,7 @@ const LinePage: NextPage = () => {
             แล้ว Add friend ส่งมาใน Botline นะ!
           </h2>
         </div>
+        <div></div>
       </div>
     </MobileFrame>
   );
