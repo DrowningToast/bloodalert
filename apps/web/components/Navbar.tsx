3b7de7fb5d8e6d74319ba19724d21def2e5dfee3
Supratouch <supratouchgussii@gmail.com>
2022-12-12 19:25:26 +0700

    update: navbar

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import Logo from "./Logo";
 
 interface Props {}
 
 const Navbar: FC = () => {
+  const router = useRouter();
+
   return (
     <div className="flex justify-between w-full px-4 pt-4 gap-x-2 items-center">
       <div className="flex items-center gap-x-4">
@@ -14,13 +17,23 @@ const Navbar: FC = () => {
       </div>
       <div className="text-[#000259] font-kanit row-start-2 col-start-2 col-span-2 flex gap-x-4 font-light text-xs">
         <Link href="/">
-          <span>หน้าแรก</span>
+          <span className={`${router.pathname === "/" ? "underline" : ""}`}>
+            หน้าแรก
+          </span>
         </Link>
         <Link href="news">
-          <span>ข่าวสาร</span>
+          <span className={`${router.pathname === "/news" ? "underline" : ""}`}>
+            ข่าวสาร
+          </span>
         </Link>
         <Link href="announcement">
-          <span>ประกาศ</span>
+          <span
+            className={`${
+              router.pathname === "/announcement" ? "underline" : ""
+            }`}
+          >
+            ประกาศ
+          </span>
         </Link>
       </div>
     </div>
