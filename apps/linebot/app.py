afc63730bfc7fde4b6c8faa4bc3b44bf8d169b43
Supratouch <supratouchgussii@gmail.com>
2022-12-14 06:18:39 +0700

    remove: unused

--- apps/linebot/app.py
+++ apps/linebot/app.py
@@ -169,19 +169,6 @@ def getLatestAnnouncements(size: int = 5):
 '''Receive an incoming announcement POST request from the server'''
 
 
-@ app.route("/announcement/new", methods=["POST"])
-def post_announcement():
-    # Parse incoming request's body
-    req = request.get_json(silent=True, force=True)
-
-    # use the data received to multicast the message to other LINE users respectively.
-    # Send(data)
-
-    # Create a new Announcement record/entity
-    add_announcement(**req)
-    return 'OK'
-
-
 @ app.route("/subscriber/<user_id>", methods=["PATCH"])
 def user(user_id):
     req_data = json.loads(request.data)
